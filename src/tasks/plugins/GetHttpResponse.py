0916e30623be09b887348730e59b522ad745b3e6
shiningbeer <shiningbeer@163.com>
2018-09-27 15:29:03 +0800

    s

--- src/tasks/plugins/GetHttpResponse.py
+++ src/tasks/plugins/GetHttpResponse.py
@@ -38,7 +38,7 @@ def parsing(txt):
 
 def scan(ip):
     protocol = 'http'
-    port = 8000
+    port = 80
     
     dict_header = {}
     http_version = 'HTTP/1.0'   #http version default is 'HTTP/1.0'
